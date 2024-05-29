103e68cc633c16522c9ba86ec07c66f789591382
Max <githubmaavel@gmail.com>
2024-05-29 06:57:44 +0600

    Сделал верстку главной странице

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export const MainPage = () => {
   return (
-    <div>MainPage</div>
+    <main className="main">
+      <div className="inner-main">
+        <div className="rows-img-text">
+          <div className="text-main">
+            <h1 className="title-main">
+            Отличное  решение для  вашего бизнеса
+            </h1>
+            <p className="decr-title-main">
+            Наша команда готова воплатить в жизнь ваши идеи  и предложить качественное решение для вашего бизнеса
+            </p>
+            <div className="btn-main-block">
+              <button className="main-btn">Продолжить</button>
+            </div>
+          </div>
+          <div className="img-main-block">
+            <img src="" alt="Элюстрация роста" />
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
